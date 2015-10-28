a7ffee737d5317db7af4a3a588357c9600a5a368
Tyler <tylerscottcrosse@gmail.com>
2015-10-28 09:44:53 -0400

    mostly updated contents of cards - not sure about other edits haha

--- js/script.js
+++ js/script.js
@@ -1,97 +1,97 @@
 var deck = [{ //1
     cardnumber: "",
-    term: "Term 1",
-    definition: "def1 lachesism n. the desire to be struck by disaster—to survive a plane crash, to lose everything in a fire, to plunge over a waterfall—which would put a kink in the smooth arc of your life, and forge it into something hardened and flexible and sharp, not just a stiff prefabricated beam that barely covers the gap between one end of your life and the other.",
+    term: "Occhiolism",
+    definition: "n. the awareness of the smallness of your perspective, by which you couldn’t possibly draw any meaningful conclusions at all, about the world or the past or the complexities of culture, because although your life is an epic and unrepeatable anecdote, it still only has a sample size of one, and may end up being the control for a much wilder experiment happening in the next room.",
     answered: false,
     correct: true
   }, { //2
     cardnumber: "",
-    term: "Term 2",
-    definition: "def 2 n. the tendency to give up trying to talk about an experience because people are unable to relate to it—whether through envy or pity or simple foreignness—which allows it to drift away from the rest of your life story, until the memory itself feels out of place, almost mythical, wandering restlessly in the fog, no longer even looking for a place to land.",
+    term: "Onism",
+    definition: "n. the frustration of being stuck in just one body, that inhabits only one place at a time, which is like standing in front of the departures screen at an airport, flickering over with strange place names like other people’s passwords, each representing one more thing you’ll never get to see before you die—and all because, as the arrow on the map helpfully points out, you are here.",
     answered: false,
     correct: true
   }, { //3
     cardnumber: "",
-    term: "Term 3",
-    definition: "def 3 n. the habit of forgetting how important someone is to you until you see them again in person, making you wish your day would begin with a “previously on” recap of your life’s various plot arcs, and end with “to be continued…” after those will-they-won’t-they cliffhanger episodes that air just before the show goes back into months of repeats.",
+    term: "Liberosis",
+    definition: "n. the desire to care less about things—to loosen your grip on your life, to stop glancing behind you every few steps, afraid that someone will snatch it from you before you reach the end zone—rather to hold your life loosely and playfully, like a volleyball, keeping it in the air, with only quick fleeting interventions, bouncing freely in the hands of trusted friends, always in play.",
     answered: true,
     correct: true
   }, { //4
     cardnumber: "",
-    term: "Term 4",
-    definition: "def 4 n. fear that your connections with people are ultimately shallow, that although your relationships feel congenial at the time, an audit of your life would produce an emotional safety deposit box of low-interest holdings and uninvested windfall profits, which will indicate you were never really at risk of joy, sacrifice or loss.",
+    term: "Vemödalen",
+    definition: "n. the frustration of photographing something amazing when thousands of identical photos already exist—the same sunset, the same waterfall, the same curve of a hip, the same closeup of an eye—which can turn a unique subject into something hollow and pulpy and cheap, like a mass-produced piece of furniture you happen to have assembled yourself.",
     answered: false,
     correct: true
   }, { //5
     cardnumber: "",
-    term: "Term 5",
-    definition: "def 5 n. the realization that each random passerby is living a life as vivid and complex as your own—populated with their own ambitions, friends, routines, worries and inherited craziness—an epic story that continues invisibly around you like an anthill sprawling deep underground, with elaborate passageways to thousands of other lives that you’ll never know existed, in which you might appear only once, as an extra sipping coffee in the background, as a blur of traffic passing on the highway, as a lighted window at dusk.",
+    term: "Vellichor",
+    definition: "n. the strange wistfulness of used bookstores, which are somehow infused with the passage of time—filled with thousands of old books you’ll never have time to read, each of which is itself locked in its own era, bound and dated and papered over like an old room the author abandoned years ago, a hidden annex littered with thoughts left just as they were on the day they were captured.",
     answered: false,
     correct: true
   }, { //6
     cardnumber: "",
-    term: "Term 6",
-    definition: "def 6 n. the frustration of photographing something amazing when thousands of identical photos already exist—the same sunset, the same waterfall, the same curve of a hip, the same closeup of an eye—which can turn a unique subject into something hollow and pulpy and cheap, like a mass-produced piece of furniture you happen to have assembled yourself.",
+    term: "Rückkehrunruhe",
+    definition: "n. the feeling of returning home after an immersive trip only to find it fading rapidly from your awareness—to the extent you have to keep reminding yourself that it happened at all, even though it felt so vivid just days ago—which makes you wish you could smoothly cross-dissolve back into everyday life, or just hold the shutter open indefinitely and let one scene become superimposed on the next, so all your days would run together and you’d never have to call cut.",
     answered: false,
     correct: true
   }, { //7
     cardnumber: "",
-    term: "Term 7",
-    definition: "def 7 n. the strange wistfulness of used bookstores, which are somehow infused with the passage of time—filled with thousands of old books you’ll never have time to read, each of which is itself locked in its own era, bound and dated and papered over like an old room the author abandoned years ago, a hidden annex littered with thoughts left just as they were on the day they were captured.",
+    term: "Adronitis",
+    definition: "n. frustration with how long it takes to get to know someone—spending the first few weeks chatting in their psychological entryway, with each subsequent conversation like entering a different anteroom, each a little closer to the center of the house—wishing instead that you could start there and work your way out, exchanging your deepest secrets first, before easing into casualness, until you’ve built up enough mystery over the years to ask them where they’re from, and what they do for a living.",
     answered: false,
     correct: true
   }, { //8
     cardnumber: "",
-    term: "Term 8",
-    definition: "def 8 n. the desire to care less about things—to loosen your grip on your life, to stop glancing behind you every few steps, afraid that someone will snatch it from you before you reach the end zone—rather to hold your life loosely and playfully, like a volleyball, keeping it in the air, with only quick fleeting interventions, bouncing freely in the hands of trusted friends, always in play.",
+    term: "Anecdoche",
+    definition: "n. a conversation in which everyone is talking but nobody is listening, simply overlaying disconnected words like a game of Scrabble, with each player borrowing bits of other anecdotes as a way to increase their own score, until we all run out of things to say.",
     answered: false,
     correct: true
   }, { //9
     cardnumber: "",
-    term: "Term 9",
-    definition: "def9 lachesism n. the desire to be struck by disaster—to survive a plane crash, to lose everything in a fire, to plunge over a waterfall—which would put a kink in the smooth arc of your life, and forge it into something hardened and flexible and sharp, not just a stiff prefabricated beam that barely covers the gap between one end of your life and the other.",
+    term: "Gnossienne",
+    definition: "n. a moment of awareness that someone you’ve known for years still has a private and mysterious inner life, and somewhere in the hallways of their personality is a door locked from the inside, a stairway leading to a wing of the house that you’ve never fully explored—an unfinished attic that will remain maddeningly unknowable to you, because ultimately neither of you has a map, or a master key, or any way of knowing exactly where you stand.",
     answered: false,
     correct: true
   }, { //10
     cardnumber: "",
-    term: "Term 10",
-    definition: "def 10 n. the tendency to give up trying to talk about an experience because people are unable to relate to it—whether through envy or pity or simple foreignness—which allows it to drift away from the rest of your life story, until the memory itself feels out of place, almost mythical, wandering restlessly in the fog, no longer even looking for a place to land.",
+    term: "énouement",
+    definition: "n. the bittersweetness of having arrived here in the future, where you can finally get the answers to how things turn out in the real world—who your baby sister would become, what your friends would end up doing, where your choices would lead you, exactly when you’d lose the people you took for granted—which is priceless intel that you instinctively want to share with anybody who hadn’t already made the journey, as if there was some part of you who had volunteered to stay behind, who was still stationed at a forgotten outpost somewhere in the past, still eagerly awaiting news from the front.",
     answered: false,
     correct: true
   }, { //11
     cardnumber: "",
-    term: "Term 11",
-    definition: "def 11 n. the habit of forgetting how important someone is to you until you see them again in person, making you wish your day would begin with a “previously on” recap of your life’s various plot arcs, and end with “to be continued…” after those will-they-won’t-they cliffhanger episodes that air just before the show goes back into months of repeats.",
+    term: "Degrassé",
+    definition: "adj. entranced and unsettled by the vastness of the universe, experienced in a jolt of recognition that the night sky is not just a wallpaper but a deeply foreign ocean whose currents are steadily carrying off all other castaways, who share our predicament but are already well out of earshot—worlds and stars who would’ve been lost entirely except for the scrap of light they were able to fling out into the dark, a message in a bottle that’s only just now washing up in the Earth’s atmosphere, an invitation to a party that already ended a million years ago.",
     answered: false,
     correct: true
   }, { //12
     cardnumber: "",
-    term: "Term 12",
-    definition: "def 12 n. fear that your connections with people are ultimately shallow, that although your relationships feel congenial at the time, an audit of your life would produce an emotional safety deposit box of low-interest holdings and uninvested windfall profits, which will indicate you were never really at risk of joy, sacrifice or loss.",
+    term: "Ellipsism",
+    definition: "n. sadness that you’ll never be able to know how history will turn out, that you’ll dutifully pass on the joke of being alive without ever learning the punchline—the name of the beneficiary of all human struggle, the sum of the final payout of every investment ever made in the future—which may not suit your sense of humor anyway and will probably involve how many people it takes to change a lightbulb.",
     answered: false,
     correct: true
   }, { //13
     cardnumber: "",
-    term: "Term 13",
-    definition: "def 13 n. the realization that each random passerby is living a life as vivid and complex as your own—populated with their own ambitions, friends, routines, worries and inherited craziness—an epic story that continues invisibly around you like an anthill sprawling deep underground, with elaborate passageways to thousands of other lives that you’ll never know existed, in which you might appear only once, as an extra sipping coffee in the background, as a blur of traffic passing on the highway, as a lighted window at dusk.",
+    term: "Lethobenthos",
+    definition: "n. the habit of forgetting how important someone is to you until you see them again in person, making you wish your day would begin with a “previously on” recap of your life’s various plot arcs, and end with “to be continued…” after those will-they-won’t-they cliffhanger episodes that air just before the show goes back into months of repeats.",
     answered: false,
     correct: true
   }, { //14
     cardnumber: "",
-    term: "Term 14",
-    definition: "def 14 n. the frustration of photographing something amazing when thousands of identical photos already exist—the same sunset, the same waterfall, the same curve of a hip, the same closeup of an eye—which can turn a unique subject into something hollow and pulpy and cheap, like a mass-produced piece of furniture you happen to have assembled yourself.",
+    term: "Mahpiohanzia",
+    definition: "n. the disappointment of being unable to fly, unable to stretch out your arms and vault into the air, having finally shrugged off the ballast of your own weight and ignited the fuel tank of unfulfilled desires you’ve been storing up since before you were born.",
     answered: false,
     correct: true
   }, { //15
     cardnumber: "",
-    term: "Term 15",
-    definition: "def 15 n. the strange wistfulness of used bookstores, which are somehow infused with the passage of time—filled with thousands of old books you’ll never have time to read, each of which is itself locked in its own era, bound and dated and papered over like an old room the author abandoned years ago, a hidden annex littered with thoughts left just as they were on the day they were captured.",
+    term: "Zielschmerz",
+    definition: "n. the exhilarating dread of finally pursuing a lifelong dream, which requires you to put your true abilities out there to be tested on the open savannah, no longer protected inside the terrarium of hopes and delusions that you created in kindergarten and kept sealed as long as you could, only to break in case of emergency.",
     answered: false,
     correct: true
   }, { //16
     cardnumber: "",
-    term: "Term 16",
-    definition: "def 16 n. the desire to care less about things—to loosen your grip on your life, to stop glancing behind you every few steps, afraid that someone will snatch it from you before you reach the end zone—rather to hold your life loosely and playfully, like a volleyball, keeping it in the air, with only quick fleeting interventions, bouncing freely in the hands of trusted friends, always in play.",
+    term: "Moledro",
+    definition: "n. a feeling of resonant connection with an author or artist you’ll never meet, who may have lived centuries ago and thousands of miles away but can still get inside your head and leave behind morsels of their experience, like the little piles of stones left by hikers that mark a hidden path through unfamiliar territory.",
     answered: false,
     correct: true
   }
